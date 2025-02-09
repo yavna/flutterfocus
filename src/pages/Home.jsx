4ddd23433affa507e5d5236c5b043ad2caa3517d
jiangcctv <138551486+jiangcctv@users.noreply.github.com>
2025-02-08 23:06:26 -0500

    mobile ui stable

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -418,7 +418,7 @@ function Home() {
                       <p>{stages[stage]}</p>
                     </motion.div>
                   </div>
-                  {/* Timer Input Section */}
+                  
                   <div>
                     <input
                       type="number"
@@ -461,7 +461,7 @@ function Home() {
                 </motion.div>
               )}
             </AnimatePresence>
-          </div>
+          </div> 
         </div>
 
         <div className="block blockGarden">
@@ -472,7 +472,7 @@ function Home() {
               src="/assets/tulips.jpg"
               alt="Tulips"
               style={{
-                width: "300px",
+                width: "28vw",
                 height: "auto",
                 marginTop: "250px",
               }}
