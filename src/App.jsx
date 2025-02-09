ebbe251401645bc06644e83107bcbd102d78c657
Helen Zhang <hajzhang@gmail.com>
2025-02-08 22:33:29 -0500

    Changing to custom domain name

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/flutterfocus">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calendar" element={<Calendar />} />
